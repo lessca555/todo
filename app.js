3bf5b60363de84aca4e25fd8578757b48a7ec3aa
Christian Samuel Seputra <stegenvak@gmail.com>
2023-02-27 15:07:30 +0700

    first

---
+++ app.js
@@ -0,0 +1,9 @@
+function add()
+{
+    //1. ambil nilai dari text.
+    let text = document.getElementById("new_text")
+    console.log(text.value);
+
+    //2. tambahkan text kedalam ul
+    //3. kosongkan ul ketika user menekan tombil reset
+}
